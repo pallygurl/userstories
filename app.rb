fa3636ea8e74157f35b7e1c36673f153493f0d41
Cyndi Bolton <cbolton@wvtpg.com>
2017-01-09 18:10:13 -0500

    price change

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
-require 'sinatra'
+# require 'sinatra'
 
-get '/' do
-    erb :order_form
-end
+# get '/' do
+#     erb :order_form
+# end
 
-post '/order' do
+# post '/order' do
     
-end
+# end
+
+items = ""
+
+puts "How many items do you want to purchase?"
+items = gets.chomp.to_i
+puts "What is the item's price?"
+price = gets.chomp.to_i
+puts "You have selected #{items} at $#{price}."
+semi_total_price = (items) * (price)
+puts "Your total before tax and discount is $#{semi_total_price}"
